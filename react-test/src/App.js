1c0dd0cc7d443c09ad2622258228416610dfc8e5
Blackmuji <hmi0203@naver.com>
2024-03-24 18:09:44 +0900

    new file:   react-test/public/Fullscreen.png 	new file:   react-test/public/Fullscreen_black.png 	new file:   react-test/public/animation.html 	modified:   react-test/public/index.html 	deleted:    react-test/public/logo192.png 	deleted:    react-test/public/logo512.png 	modified:   react-test/src/App.css 	modified:   react-test/src/App.js 	new file:   react-test/src/animation.css 	modified:   react-test/src/index.css

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // 필요한 CSS 파일을 import 해주세요
 
 function App() {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 배경 이미지 */}
+      <div
+        className="background"
+        style={{
+          backgroundImage: "url('/public/Fullscreen_black.png')",
+          opacity: showLoginForm ? 1 : 0, // 로그인 폼이 보일 때 배경이미지의 투명도를 조절합니다.
+        }}
+        onClick={() => setShowLoginForm(!showLoginForm)} // 배경을 클릭하면 로그인 폼을 토글합니다.
+      ></div>
+      
+      {/* 로그인 폼 */}
+      {showLoginForm && (
+        <div className="loginForm">
+          <h2>로그인</h2>
+          <form>
+            <label htmlFor="username">사용자명:</label><br />
+            <input type="text" id="username" name="username" /><br />
+            <label htmlFor="password">비밀번호:</label><br />
+            <input type="password" id="password" name="password" /><br /><br />
+            <input type="submit" value="로그인" />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
