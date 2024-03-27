69ebea552e14e7c9bbda30eb407f9e8acbe22ec4
Blackmuji <hmi0203@naver.com>
2024-03-27 16:44:59 +0900

    commit

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,41 +1,42 @@
-import React, { useState } from 'react';
-import './App.css'; // 필요한 CSS 파일을 import 해주세요
+import React, { Component } from 'react';
+import './App.css';
+import Customer from './components/Customer';
 
+const customers = [{
+  'id': '1',
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': '2',
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '고길동',
+  'birthday': '123456',
+  'gender': '남자',
+  'job': '의사'
+},
+{
+  'id': '3',
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '김길동',
+  'birthday': '112233',
+  'gender': '남자',
+  'job': '교수'
+},]
 
-function App(props) {
-  return (
-  <div className="background-image"></div>
-
-  /*<section className="login-section">
-    <div className="wrap">
-      <div className="image-wrapper">
-        <h1 className="blind">logo</h1>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {return (<Customer id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job}/> ); })
+        }
       </div>
-      <div class="content-wrapper">
-        <form id="signup">
-          <h1>Login</h1>
-          <div class="input-box">
-            <input type="text" placeholder="아이디" required>
-            <i class='bx bxs-user'></i>
-          </div>
-          <div class="input-box">
-            <input type="password" placeholder="비밀번호" required>
-            <i class='bx bxs-lock'></i>
-          </div>
-          <div class="remember-forgot">
-            <label><input type="checkbox"> Remember me</label>
-          </div>
-          <div class="login_wrap">
-            <a href="#">아이디 찾기</a>
-            <a href="#">비밀번호 찾기</a>
-            <a href="#">회원가입</a>
-          </div>
-          <button type="submit" class="btn">로그인</button>
-        </form>
-        <div>
-        </div>
-  </section>*/
-  );
+    )
+  };
 }
 
 export default App;
