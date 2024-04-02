61606ac5a0554658ebf68e0669d5f64822fa595d
Blackmuji <hmi0203@naver.com>
2024-04-02 15:51:09 +0900

    modified:   react-test/src/css/MainPage.css 	modified:   react-test/src/css/RegisterForm.css 	modified:   react-test/src/pages/App.js 	new file:   react-test/src/pages/LoginForm.js 	modified:   react-test/src/pages/MainPage.js

--- react-test/src/pages/App.js
+++ react-test/src/pages/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import '../css/App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
 import MainPage from './MainPage';
 import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 import Test from './Test';
 
 
@@ -12,8 +13,10 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="" element={<MainPage />}/>
-          <Route path="/register" element={<RegisterForm />}/>
+          <Route path="/" element={<MainPage />}>
+            <Route path="/register" element={<RegisterForm />}/>
+            <Route path="/login" element={<LoginForm />}/>
+          </Route>
           <Route path="/test" element={<Test />}/>
         </Routes>
       </div>
--- react-test/src/pages/MainPage.js
+++ react-test/src/pages/MainPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import '../css/MainPage.css';
 
 const MainPage = () => {
@@ -9,9 +9,14 @@ const MainPage = () => {
       navigate('/register');
     };
 
+    useEffect(() => {
+        document.querySelector('.background-image').classList.add('change-background');
+      }, []);
+
     return (
         <div>
             <div className="background-image" onClick={handleClick}></div>
+            <Outlet />
         </div>
     );
 };
