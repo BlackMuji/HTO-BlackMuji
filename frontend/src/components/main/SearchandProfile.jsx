4bb1bdda4c4f55084fcd6ae0aeb9216076d35320
BlackMuji <kmi00203@gmail.com>
2024-10-22 17:37:08 +0900

    modified:   frontend/src/App.jsx 	deleted:    frontend/src/assets/scss/section/_exp.scss 	deleted:    frontend/src/assets/scss/section/_header.scss 	modified:   frontend/src/assets/scss/section/_layout.scss 	deleted:    frontend/src/assets/scss/section/_main.scss 	deleted:    frontend/src/assets/scss/section/_rank.scss 	deleted:    frontend/src/assets/scss/section/_search.scss 	new file:   frontend/src/assets/scss/section/_searchandprofile.scss 	new file:   frontend/src/assets/scss/section/_sidebar.scss 	modified:   frontend/src/assets/scss/style.scss 	modified:   frontend/src/components/contest/ContestDetail.jsx 	deleted:    frontend/src/components/header/Logo.jsx 	deleted:    frontend/src/components/header/Logo2.jsx 	deleted:    frontend/src/components/header/Menu.jsx 	deleted:    frontend/src/components/header/Sns.jsx 	renamed:    frontend/src/components/section/Footer.jsx -> frontend/src/components/main/Footer.jsx 	new file:   frontend/src/components/main/Main.jsx 	new file:   frontend/src/components/main/SearchandProfile.jsx 	new file:   frontend/src/components/main/Sidebar.jsx 	renamed:    frontend/src/components/header/Logo1.jsx -> frontend/src/components/main/header/Logo.jsx 	renamed:    frontend/src/components/section/Profile.jsx -> frontend/src/components/main/header/Profile.jsx 	new file:   frontend/src/components/main/header/Search.jsx 	deleted:    frontend/src/components/section/Header.jsx 	deleted:    frontend/src/components/section/Main.jsx 	deleted:    frontend/src/components/section/Search.jsx 	deleted:    frontend/src/components/section/SearchAndProfile.jsx 	modified:   frontend/src/pages/ContestPage.jsx 	new file:   frontend/src/pages/MainPage.jsx 	modified:   frontend/src/pages/RankingPage.jsx

---
+++ frontend/src/components/main/SearchandProfile.jsx
@@ -0,0 +1,13 @@
+import Search from './header/Search';
+import Profile from './header/Profile';
+
+const SearchandProfile = () => {
+    return (
+        <div className='search-profile-container'>
+            <Search />
+            <Profile />
+        </div>
+    )
+}
+
+export default SearchandProfile;
