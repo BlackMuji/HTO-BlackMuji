4d2700b5ceca2312355579becfd079c127857994
BlackMuji <kmi00203@gmail.com>
2024-11-15 21:00:24 +0900

    new file:   frontend/src/assets/img/machines/computer0.png 	new file:   frontend/src/assets/img/machines/computer1.png 	new file:   frontend/src/assets/img/machines/computer2.png 	new file:   frontend/src/assets/img/machines/computer3.png 	new file:   frontend/src/assets/img/machines/computer4.png 	new file:   frontend/src/assets/img/machines/computer5.png 	new file:   frontend/src/assets/img/machines/computer6.png 	modified:   frontend/src/assets/scss/contest/ContestList.module.scss 	new file:   frontend/src/assets/scss/leaderboard/ContestUserInfo.module.scss 	modified:   frontend/src/assets/scss/machine/AddMachineForm.scss 	modified:   frontend/src/assets/scss/machine/MachineBanner.scss 	modified:   frontend/src/assets/scss/machine/MachineDetail.scss 	modified:   frontend/src/assets/scss/machine/MachineList.scss 	modified:   frontend/src/assets/scss/mypage/mypage.scss 	modified:   frontend/src/components/contest/ContestList.tsx 	new file:   frontend/src/components/leaderboard/ContestUserInfo.tsx 	modified:   frontend/src/components/leaderboard/CurrentUserInfo.tsx 	modified:   frontend/src/components/leaderboard/LeaderboardTable.tsx 	modified:   frontend/src/components/machine/AddMachineForm.tsx 	modified:   frontend/src/components/machine/MachineBanner.tsx 	modified:   frontend/src/components/machine/MachineDetail.tsx 	modified:   frontend/src/components/machine/MachineList.tsx 	modified:   frontend/src/components/main/Profile.tsx 	modified:   frontend/src/components/mypage/PersonalInfoForm.tsx 	modified:   frontend/src/pages/contest/ContestDetailPage.tsx 	modified:   frontend/src/pages/leaderboard/LeaderBoardPage.tsx 	modified:   frontend/src/pages/machine/MachineListPage.tsx 	modified:   frontend/src/types/CurrentUser.ts 	new file:   frontend/src/utils/avatars.tsx

--- frontend/src/components/main/Profile.tsx
+++ frontend/src/components/main/Profile.tsx
@@ -5,10 +5,6 @@ import { AuthUserContext } from '../../contexts/AuthUserContext';
 import styles from '../../assets/scss/section/_profile.module.scss';
 import { useProfileContext } from '../../contexts/ProfileContext';
 
-
-import { FaArrowRightToBracket } from "react-icons/fa6";
-
-import user_default from '../../assets/img/icon/profile_default.png';
 import down_arrow from '../../assets/img/icon/down_arrow.svg';
 import up_arrow from '../../assets/img/icon/up_arrow.svg';
 import chat from '../../assets/img/icon/chat_icon.svg';
@@ -16,6 +12,9 @@ import setting from '../../assets/img/icon/setting_icon.svg';
 import personal_info from '../../assets/img/icon/personal_info.svg';
 import darkmode_icon from '../../assets/img/icon/darkmode_icon.svg';
 import darkmode_switch from '../../assets/img/icon/darkmode_switch.svg';
+import arrow_left from '../../assets/img/icon/Arrow-Left.svg';
+import { Avatar } from '@mui/material';
+import { avatarBackgroundColors, getAvatarColorIndex } from '../../utils/avatars';
 
 interface MenuItemProps {
     onClick?: () => void;
@@ -50,13 +49,21 @@ const Profile: React.FC = () => {
             }
         }
     };
+    const avatarColorIndex = getAvatarColorIndex(currentUser?.username || '');
+    const avatarBgColor = avatarBackgroundColors[avatarColorIndex]; 
 
     return (
         <div className={styles.userinfoDark}>
             <div className={styles.userinfoDark_container}>
                 <div className={styles.userinfo}>
                     <div className={styles.rectangleParent}>
-                        <img className={styles.userIcon} alt="" src={currentUser?.avatar || user_default} />
+                        <Avatar
+                            sx={{
+                                backgroundColor: avatarBgColor, width: 50, height: 50
+                            }}
+                        >
+                            {currentUser?.username?.charAt(0).toUpperCase() || ''}
+                        </Avatar>
                         <div className={styles.userinfoContainer}>
                             <div className={styles.userName}>{currentUser?.username || 'Guest'}</div>
                             <div className={styles.userEmail}>{currentUser?.email || 'Guest'}</div>
@@ -66,41 +73,41 @@ const Profile: React.FC = () => {
                         <img className={styles.userinfoButtonIcon} alt="" src={isProfileCollapsed ? down_arrow : up_arrow} />
                     </div>
                 </div>
-            </div>
-            <div className={`${styles.userMenu} ${isProfileCollapsed ? styles.collapsed : styles.expanded}`}>
-                <li className={styles.settings}>
-                    <div className={styles.settingsInner}>
-                        <div className={styles.rectangleParent}>
-                            <img className={styles.icon} alt="" src={personal_info} />
-                            <MenuItem onClick={() => navigate('/mypage')}>개인정보 수정</MenuItem>
-                        </div>
-                    </div>
-                    <div className={styles.settingsInner}>
-                        <div className={styles.rectangleParent}>
-                            <img className={styles.icon} alt="" src={chat} />
-                            <div className={styles.dashboard}>Messages</div>
-                        </div>
-                    </div>
-                    <div className={styles.settingsInner}>
-                        <div className={styles.rectangleParent}>
-                            <img className={styles.icon} alt="" src={setting} />
-                            <div className={styles.lianaParker}>Settings</div>
-                        </div>
-                    </div>
-                    <div className={styles.settingsInner}>
-                        <div className={styles.rectangleParent}>
-                            <img className={styles.icon} alt="" src={darkmode_icon} />
-                            <div className={styles.darkModeParent}>
-                                <div className={styles.darkMode}>Dark mode</div>
+                <div className={`${styles.userMenu} ${isProfileCollapsed ? styles.collapsed : ''}`}>
+                    <li className={styles.settings}>
+                        <div className={styles.settingsInner}>
+                            <div className={styles.rectangleParent}>
+                                <img className={styles.icon} alt="" src={personal_info} />
+                                <MenuItem onClick={() => navigate('/mypage')}>개인정보 수정</MenuItem>
                             </div>
                         </div>
-                        <img className={styles.switchIcon} alt="" src={darkmode_switch} />
+                        <div className={styles.settingsInner}>
+                            <div className={styles.rectangleParent}>
+                                <img className={styles.icon} alt="" src={chat} />
+                                <div className={styles.dashboard}>Messages</div>
+                            </div>
+                        </div>
+                        <div className={styles.settingsInner}>
+                            <div className={styles.rectangleParent}>
+                                <img className={styles.icon} alt="" src={setting} />
+                                <div className={styles.lianaParker}>Settings</div>
+                            </div>
+                        </div>
+                        <div className={styles.settingsInner}>
+                            <div className={styles.rectangleParent}>
+                                <img className={styles.icon} alt="" src={darkmode_icon} />
+                                <div className={styles.darkModeParent}>
+                                    <div className={styles.darkMode}>Dark mode</div>
+                                </div>
+                            </div>
+                            <img className={styles.switchIcon} alt="" src={darkmode_switch} />
+                        </div>
+                    </li>
+                    <div className={styles.userinfoDarkChild} />
+                    <div className={styles.logout}>
+                        <img className={styles.icon} alt="" src={arrow_left} />
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
                     </div>
-                </li>
-                <div className={styles.userinfoDarkChild} />
-                <div className={styles.logout}>
-                    <FaArrowRightToBracket className={styles.logout_icon} size={30} />
-                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
                 </div>
             </div>
         </div >
--- frontend/src/types/CurrentUser.ts
+++ frontend/src/types/CurrentUser.ts
@@ -1,4 +1,5 @@
 export interface CurrentUser {
+    _id: string | null;
     myRank: number | null;
     myLevel: number | null;
     myExp: number | null;
